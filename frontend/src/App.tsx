6d9cc9d33526a7b01b7600bb9fc7490ffd53356b
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2026-02-25 18:06:14 +0800

    update App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,9 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5,
-      retry: 1
-    }
-  }
+      retry: 1,
+    },
+  },
 });
 
 const App = () => (
